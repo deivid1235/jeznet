0376d16a9bf8c1b7749d392b8a82d0464d67c9b2
Gnorrs <isainores2007@gmail.com>
2026-03-02 18:14:30 -0500

    Estilos modificados del index de style

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -46,9 +46,6 @@
                     <li class="nav-item ms-lg-3 my-2 my-lg-0">
                         <a class="nav-link d-flex align-items-center" href="#">
                             INICIAR SESIÓN
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="ms-2" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-                            </svg>
                         </a>
                     </li>
 
