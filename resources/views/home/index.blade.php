19f5b0b81bfcb126852497c1e2799d20090972e0
Gnorrs <isainores2007@gmail.com>
2026-03-02 18:10:19 -0500

    Estilos modificados del index de style

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,7 +7,7 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800;900&display=swap" rel="stylesheet">
-    @vite(['resources/css/home_index.css','resources/js/app.js'])
+    @vite(['resources/css/style.css','resources/js/app.js'])
 </head>
 
 <body>
